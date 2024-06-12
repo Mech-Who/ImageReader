a8b03bc02ad0c2532893e86af161d85bf45b4a97
HuShuhan <873933169@qq.com>
2024-06-12 11:52:37 +0800

    main: Fixed 修复主程序代码错误

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
+
 from PySide6.QtWidgets import QApplication
+
+from src.gui.main_window import MainWindow
 
 # 初始化QApplication，界面展示要包含在QApplication初始化之后，结束之前
 app = QApplication(sys.argv)
 
 # 初始化并展示我们的界面组件
-window = ()
+window = MainWindow()
 window.show()
 
 # 结束QApplication
