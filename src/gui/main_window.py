6f1771379a164d2ab5a588255f609f763ff65e01
HuShuhan <873933169@qq.com>
2024-06-12 12:05:47 +0800

    main: Updated 获取目录中的文件列表功能完成

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,14 +1,77 @@
+import os
+from pathlib import Path
+from typing import List, Dict, Tuple, Union
+
 # 任何一个PySide界面程序都需要使用QApplication
 # 我们要展示一个普通的窗口，所以需要导入QWidget，用来让我们自己的类继承
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QImage, QIcon
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QGraphicsScene, QTreeWidgetItem
+from PySide6.QtCore import Qt, Slot, Signal
 
 # 导入我们生成的界面
 from src.gui.gen.Ui_MainWindow import Ui_MainWindow
+from src.config.path_config import PROJECT_ROOT, ASSETS_ROOT
+from src.utils.file import getFileType, create_top_item
 
 # 继承QWidget类，以获取其属性和方法
+
+
 class MainWindow(QMainWindow):
+    select_image = Signal()
+    select_dir = Signal()
+    select_csv = Signal()
+
     def __init__(self):
         super(MainWindow, self).__init__()
         # 设置界面为我们生成的界面
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.class_init()
+        self.ui_init()
+        self.slot_signal_init()
+
+    def class_init(self):
+        pass
+
+    def ui_init(self):
+        pass
+
+    def slot_signal_init(self):
+        pass
+
+    # 槽函数
+    @Slot()
+    def on_actionselect_image_file_triggered(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "选择图片",
+                                                   dir=str(PROJECT_ROOT),
+                                                   filter="图片文件 (*.jpg *.gif *.png *.jpeg)")
+        print(f"open file: {file_name}")
+        file_path = Path(file_name)
+        self.ui.imageLabel.setText(f"{file_path.stem}")
+        scene = QGraphicsScene()
+        scene.addPixmap(str(file_path))
+        self.ui.graphicsView.setScene(scene)
+        self.ui.graphicsView.fitInView(
+            scene.sceneRect(),
+            Qt.AspectRatioMode.KeepAspectRatio)
+        self.ui.graphicsView.show()
+
+    @Slot()
+    def on_actionselect_directory_triggered(self):
+        dir_name = QFileDialog.getExistingDirectory(
+            self, "选择图片目录", dir=str(PROJECT_ROOT))
+        # print(f"open dir: {dir_name}")
+        self.dir_path = Path(dir_name)
+        self.dir_files = [self.dir_path / file 
+                          for file in os.listdir(self.dir_path)]
+        # print(self.dir_files[0])
+        tree = self.ui.fileTreeWidget
+        top_item = QTreeWidgetItem(tree)
+        top_item.setText(0, str(self.dir_path.stem))
+        top_item.setText(1, getFileType(self.dir_path))
+        top_item.setIcon(0, QIcon(str(ASSETS_ROOT / "icon/dir-folder.png")))
+
+        self.create_top_item(top_item, self.dir_files)
+
+        tree.addTopLevelItem(top_item)
+        # tree.expandAll()
---
+++ src/gui/ui/ui_mainwindow_ui.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ui_mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QGridLayout, QHeaderView,
+    QLabel, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QTabWidget, QTreeWidget,
+    QTreeWidgetItem, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.actionselect_image_file = QAction(MainWindow)
+        self.actionselect_image_file.setObjectName(u"actionselect_image_file")
+        self.actionselect_directory = QAction(MainWindow)
+        self.actionselect_directory.setObjectName(u"actionselect_directory")
+        self.actionselect_csv_file = QAction(MainWindow)
+        self.actionselect_csv_file.setObjectName(u"actionselect_csv_file")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.graphicsView = QGraphicsView(self.centralwidget)
+        self.graphicsView.setObjectName(u"graphicsView")
+
+        self.gridLayout.addWidget(self.graphicsView, 1, 1, 1, 1)
+
+        self.imageLabel = QLabel(self.centralwidget)
+        self.imageLabel.setObjectName(u"imageLabel")
+        self.imageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.gridLayout.addWidget(self.imageLabel, 0, 1, 1, 1)
+
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.dirTab = QWidget()
+        self.dirTab.setObjectName(u"dirTab")
+        self.gridLayout_2 = QGridLayout(self.dirTab)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.fileTreeWidget = QTreeWidget(self.dirTab)
+        self.fileTreeWidget.setObjectName(u"fileTreeWidget")
+
+        self.gridLayout_2.addWidget(self.fileTreeWidget, 0, 0, 1, 1)
+
+        self.tabWidget.addTab(self.dirTab, "")
+        self.otherTab = QWidget()
+        self.otherTab.setObjectName(u"otherTab")
+        self.tabWidget.addTab(self.otherTab, "")
+
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 2, 1)
+
+        self.gridLayout.setColumnStretch(0, 1)
+        self.gridLayout.setColumnStretch(1, 3)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.fileMenu = QMenu(self.menubar)
+        self.fileMenu.setObjectName(u"fileMenu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.fileMenu.menuAction())
+        self.fileMenu.addAction(self.actionselect_image_file)
+        self.fileMenu.addAction(self.actionselect_directory)
+        self.fileMenu.addAction(self.actionselect_csv_file)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionselect_image_file.setText(QCoreApplication.translate("MainWindow", u"select image file", None))
+        self.actionselect_directory.setText(QCoreApplication.translate("MainWindow", u"select directory", None))
+        self.actionselect_csv_file.setText(QCoreApplication.translate("MainWindow", u"select csv file", None))
+        self.imageLabel.setText(QCoreApplication.translate("MainWindow", u"\u56fe\u7247Label", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.dirTab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.otherTab), QCoreApplication.translate("MainWindow", u"Tab 2", None))
+        self.fileMenu.setTitle(QCoreApplication.translate("MainWindow", u"\u6587\u4ef6", None))
+    # retranslateUi
+
