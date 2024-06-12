d22fc6e2f0deeb1a7d8a60baf9b38dcca5fcc9e8
HuShuhan <873933169@qq.com>
2024-06-12 11:27:36 +0800

    main: Added 添加路径配置

---
+++ src/config/path_config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(r"D:\Project\MyProjects\ImageReader")
+IMG_ROOT = Path(r"D:\Project\MyProjects\ImageReader\assets")
+SRC_ROOT = Path(r"D:\Project\MyProjects\ImageReader\src")
+UI_ROOT = Path(r"D:\Project\MyProjects\ImageReader\src\gui\ui")
+GEN_ROOT = Path(r"D:\Project\MyProjects\ImageReader\src\gui\gen")
